06f420bc59c2e1488977bdf548438ec4e5bbcc5a
Idan Arye <idanarye@weka.io>
2023-11-09 19:37:15 +0200

    Use my broken(?) GLB file

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
+Press Spacebar to start the animation
+
 https://idanarye.github.io/demonstrate-bevy-0.12-animation-bug/bevy_github_ci_template
 
--- src/main.rs
+++ src/main.rs
@@ -19,8 +19,12 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
         transform: Transform::from_xyz(1.0, 2.0, 10.0).looking_at(Vec3::ZERO, Vec3::Y),
         ..Default::default()
     });
+    commands.spawn(PointLightBundle {
+        transform: Transform::from_xyz(0.0, 5.0, 0.0),
+        ..default()
+    });
     commands.spawn(SceneBundle {
-        scene: asset_server.load("thing.glb#Scene0"),
+        scene: asset_server.load("player.glb#Scene0"),
         ..Default::default()
     });
 }
@@ -30,19 +34,9 @@ fn start_animation(
     mut animation_players: Query<&mut AnimationPlayer>,
     asset_server: Res<AssetServer>,
 ) {
-    for keycode in keyboard.get_just_pressed() {
-        let animation_clip = match keycode {
-            KeyCode::Key1 => asset_server.load("thing.glb#Animation0"),
-            KeyCode::Key2 => asset_server.load("thing.glb#Animation1"),
-            _ => continue,
-        };
+    if keyboard.just_pressed(KeyCode::Space) {
         for mut animation_player in animation_players.iter_mut() {
-            animation_player
-                .start(animation_clip.clone())
-                .set_speed(1.0);
-            if keyboard.any_pressed([KeyCode::ControlLeft, KeyCode::ControlRight]) {
-                animation_player.repeat();
-            }
+            animation_player.start(asset_server.load("player.glb#Animation6"));
         }
     }
 }
