16859da62c8ac4bd674e252ca66f10e44c83cc06
Idan Arye <idanarye@weka.io>
2023-11-09 19:25:35 +0200

    Try making an animation example

--- src/main.rs
+++ src/main.rs
@@ -10,13 +10,39 @@ fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_systems(Startup, setup)
+        .add_systems(Update, start_animation)
         .run();
 }
 
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
-    commands.spawn(Camera2dBundle::default());
-    commands.spawn(SpriteBundle {
-        texture: asset_server.load("icon.png"),
+    commands.spawn(Camera3dBundle {
+        transform: Transform::from_xyz(1.0, 2.0, 10.0).looking_at(Vec3::ZERO, Vec3::Y),
         ..Default::default()
     });
+    commands.spawn(SceneBundle {
+        scene: asset_server.load("thing.glb#Scene0"),
+        ..Default::default()
+    });
+}
+
+fn start_animation(
+    keyboard: Res<Input<KeyCode>>,
+    mut animation_players: Query<&mut AnimationPlayer>,
+    asset_server: Res<AssetServer>,
+) {
+    for keycode in keyboard.get_just_pressed() {
+        let animation_clip = match keycode {
+            KeyCode::Key1 => asset_server.load("thing.glb#Animation0"),
+            KeyCode::Key2 => asset_server.load("thing.glb#Animation1"),
+            _ => continue,
+        };
+        for mut animation_player in animation_players.iter_mut() {
+            animation_player
+                .start(animation_clip.clone())
+                .set_speed(1.0);
+            if keyboard.any_pressed([KeyCode::ControlLeft, KeyCode::ControlRight]) {
+                animation_player.repeat();
+            }
+        }
+    }
 }
